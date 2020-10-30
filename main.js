9a5be0686b3422cccb7e1cfdf1dd22a25a394a22
Roma Levsha <rm.levsha@gmail.com>
2020-10-30 11:11:20 +0300

    fix: correct quotes

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-const slides = document.querySelectorAll(".slide");
-const FullSize = "open";
-const ReplaceText = "open-active";
+const slides = document.querySelectorAll('.slide');
+const FullSize = 'open';
+const ReplaceText = 'open-active';
 
 function toggleOpen() {
-  console.log(this);
   this.classList.toggle(FullSize);
 }
 
 function toggleActive(e) {
-  console.log(e);
-  if (e.propertyName.includes("flex")) {
+  if (e.propertyName.includes('flex')) {
     this.classList.toggle(ReplaceText);
   }
 }
 //function run work onclick, not transitioned
 /* function removeActive() {
-  if (this.classList.contains("open-active")) {
-    this.classList.remove("open-active");
+  if (this.classList.contains('open-active')) {
+    this.classList.remove('open-active');
   } else {
-    this.classList.add("open-active");
+    this.classList.add('open-active');
   }
 }
 
-slides.forEach((slide) => slide.addEventListener("click", removeActive));
+slides.forEach((slide) => slide.addEventListener('click', removeActive));
  */
 
-slides.forEach((slide) => slide.addEventListener("click", toggleOpen));
+slides.forEach((slide) => slide.addEventListener('click', toggleOpen));
 slides.forEach((slide) =>
-  slide.addEventListener("transitionend", toggleActive)
+  slide.addEventListener('transitionend', toggleActive)
 );
