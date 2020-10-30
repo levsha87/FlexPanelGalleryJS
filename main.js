e086bf4db44a698c19191b3e3b2faea074d13807
Roma Levsha <rm.levsha@gmail.com>
2020-10-30 22:09:52 +0300

    feat: replace function, add eventListener

--- main.js
+++ main.js
@@ -1,29 +1,12 @@
-const slides = document.querySelectorAll('.slide');
-const FULL_SIZE = 'open';
-const REPLACE_TEXT = 'open-active';
+const FULL_SIZE = "open";
+const REPLACE_TEXT = "open-active";
 
-function toggleOpen() {
-  this.classList.toggle(FULL_SIZE);
-}
+document.querySelector('.slides').addEventListener('click', function (e) {
+  e.target.closest('div').classList.toggle(FULL_SIZE);
+});
 
-function toggleActive(e) {
+document.querySelector('.slides').addEventListener('transitionend', function (e) {
   if (e.propertyName.includes('flex')) {
-    this.classList.toggle(REPLACE_TEXT);
+    e.target.closest('div').classList.toggle(REPLACE_TEXT);
   }
-}
-//function run work onclick, not transitioned
-/* function removeActive() {
-  if (this.classList.contains('open-active')) {
-    this.classList.remove('open-active');
-  } else {
-    this.classList.add('open-active');
-  }
-}
-
-slides.forEach((slide) => slide.addEventListener('click', removeActive));
- */
-
-slides.forEach((slide) => slide.addEventListener('click', toggleOpen));
-slides.forEach((slide) =>
-  slide.addEventListener('transitionend', toggleActive)
-);
+});
