79abc9a34970de15382c143d2b7ef27c294de7cc
Roma Levsha <rm.levsha@gmail.com>
2020-10-31 09:52:11 +0300

    feat: realize new function click on new element, remove class from old element

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const FULL_SIZE = "open";
-const REPLACE_TEXT = "open-active";
+const FULL_SIZE = 'open';
+const REPLACE_TEXT = 'open-active';
 
 document.querySelector('.slides').addEventListener('click', function (e) {
-  e.target.closest('div').classList.toggle(FULL_SIZE);
+  const parentDiv = e.target.closest('div');
+  if (!parentDiv.classList.contains(FULL_SIZE)) {
+    for (let i = 0; i < document.querySelector('.slides').children.length; i++) {
+      document.querySelector('.slides').children[i].classList.remove('open');
+    }
+    parentDiv.classList.add(FULL_SIZE);
+  } else {
+    parentDiv.classList.remove(FULL_SIZE);
+  }
 });
 
 document.querySelector('.slides').addEventListener('transitionend', function (e) {
-  if (e.propertyName.includes('flex')) {
-    e.target.closest('div').classList.toggle(REPLACE_TEXT);
-  }
-});
+    if (e.propertyName.includes('flex')) {
+      e.target.closest('div').classList.toggle(REPLACE_TEXT);
+    }
+  });
